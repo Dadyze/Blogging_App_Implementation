e7ee5dd7b3efd8e1bc701182842478263ed42876
Dadyze <adin.jahic@gmail.com>
2022-01-28 00:38:55 +0100

    app sent

---
+++ routes/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+const Story = require('../models/Story')
+
+// @desc    Login/Landing page
+// @route   GET /
+
+router.get('/', ensureGuest,async (req,res)=>{
+  try {
+    const stories = await Story.find({ status: 'public' })
+      .populate("user")
+      .sort({ createdAt: 'desc' })
+      .lean()
+
+    res.render('stories/guest', {
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+
+// @desc    Login/Landing page
+// @route   GET /login
+router.get('/login', ensureGuest, (req, res) => {
+  res.render('login', {
+    layout: 'login',
+  })
+})
+
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    var stories;
+    if(req.user.id == process.env.ADMIN_ID){
+       stories = await Story.find(_ => true).lean();
+    }else{
+       stories = await Story.find({ user: req.user.id }).lean()     
+    }
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+module.exports = router
